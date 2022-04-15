3294bf080ddae5c9556d6665c647bd80cbacd7f0
ShogoOnda <duo0116@gmail.com>
2022-04-15 15:14:27 +0000

    [Add] データベース&コントローラ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "5a10485846024f508d00b65a0cdee2e5.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
