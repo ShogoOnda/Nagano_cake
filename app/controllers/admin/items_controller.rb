4c1329a8f5d4302320a24e1c006c79eec94daefc
ShogoOnda <duo0116@gmail.com>
2022-04-22 03:26:25 +0000

    [Add] 商品index,show,editページの追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,30 @@
 class Admin::ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(image_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      @items = Item.all
+      render :index
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  private
+
+  def item_images_params
+    params.require(:item).permit(:images, :name, :introduction, :genre, :price, :is_active)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,36 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
+  scope module: :public do
+    root :to =>"homes#top"
+    get "/about"=>"homes#about", as: 'about'
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update]
+    get "/customers/unsubscribe"=>"customers#unsubscribe", as: 'unsubscribe'
+    patch "/customers/withdraw"=>"customers#withdraw", as: 'withdraw'
+    resources :cart_items, only: [:index, :update, :detroy, :create]
+    delete "/cart_items/destroy_all" => "cart_items#destroy_all", as: 'destroy_all'
+    resources :orders, only: [:new, :create, :index, :show]
+    post "/orders/confirm"=>"orders#confirm", as: 'confirm'
+    get "/orders/complete"=>"orders#complete", as: 'complete'
+    resources :addresses, only: [:index, :edit, :create, :update, :detroy]
+  end
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    root :to =>"homes#top"
+    get "admin/home/about"=>"admin/homes#about", as: 'about'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :new, :create, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+
 end
